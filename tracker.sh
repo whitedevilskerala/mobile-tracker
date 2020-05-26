46aa13ac818f6a357d41173c2e7b98284d3bc116
whitedevilskerala <65941597+whitedevilskerala@users.noreply.github.com>
2020-05-26 14:21:57 +0530

    New updated tracker coded by whitedevilskerala
    We are kerala cyber team

---
+++ tracker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+   green="\e[92mLight green"
+
+    figlet white devils tracker
+   echo "this tool maded by whitedevils kerala cyber team you can contact us on instagram  @white_devils_hackers
+    
+   echo -e $green
+
+   echo "enter your api key:"
+
+   read api key
+
+   echo "enter your target mobile number with country code"
+
+   read mobile number
+
+   curl -s http://apilayer.net/api/validate?access_key=api key+mobile number
